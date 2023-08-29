be6b3bd9eef9c7638c06b45754b3ce22335be40c
Pierre DGR <pierredgr@MacBook-Air-de-Pierre.local>
2023-08-29 17:47:28 +0200

    test

--- test.py
+++ test.py
@@ -4,15 +4,16 @@ import json
 import base64
 import time
 import sys
+import argparse
 
 CHUNK_SIZE = 4096  # 4KB, vous pouvez ajuster cette taille selon vos besoins
-URL = 'http://localhost:8000/upload/'
-ASK_URL = 'http://localhost:8000/ask/'
+#URL = 'http://localhost:8000/upload/'
+#ASK_URL = 'http://localhost:8000/ask/'
 
 REMOTE_URL = 'https://delosaskpdf-b71e417edc56.herokuapp.com/upload/'
 REMOTE_ASK_URL = 'https://delosaskpdf-b71e417edc56.herokuapp.com/ask/'
 
-PATH = "/Users/pierredgr/Documents/Business/Actuels/Delos/NUKEMA/CCTP.pdf" # Remplacer par un fichier test
+PATH = "/Users/pierredgr/Documents/Business/Actuels/Delos/NUKEMA/2023-S-0020_CCTP.pdf" # Remplacer par un fichier test
 
 def send_chunks(file_path : str = PATH):
     print("Beginning sending the doc")
@@ -20,7 +21,7 @@ def send_chunks(file_path : str = PATH):
     with open(file_path, 'rb') as pdf_file:
         file_size = os.path.getsize(file_path)
         file_info = {
-                'user_id' : "1345",
+                'user_id' : "1346",
                 'title' : "test_file",
                 "type":".pdf",
                 "size":str(file_size),
@@ -50,7 +51,7 @@ def ask():
     question = input("Entrez votre question : ")
     chat_buffer = str([{"user": "de quoi parle ce document ? ", "system": "Ce document parle d'un marché de nettoyage pour le parking Brauhauban de la ville de Tarbes. Il décrit les différentes prestations de nettoyage demandées, telles que le nettoyage des bureaux, des sanitaires, des vitres, des surfaces de roulement du parking, etc. Le document précise également les responsabilités du prestataire en termes de stockage et de traitement des déchets, ainsi que les formalités administratives à suivre après chaque intervention. Il mentionne également les moyens nécessaires pour assurer les prestations, tels que le personnel et le matériel utilisé. Enfin, le document énonce les clauses techniques particulières et décrit les caractéristiques du parking et des espaces à nettoyer."}])
     question_info = {
-        "user_id" : "1345",
+        "user_id" : "1346",
         "question" : question,
         "chat_buffer" : chat_buffer,
     }
@@ -66,4 +67,14 @@ def ask():
 
     return
 
-ask()
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Exécute différentes fonctions.")
+    parser.add_argument("-function", choices=["upload", "ask"], help="La fonction à exécuter")
+    args = parser.parse_args()
+    
+    if args.function == "upload":
+        send_chunks()
+    elif args.function == "ask":
+        ask()
+    else:
+        print("Veuillez spécifier une fonction valide à exécuter.")
